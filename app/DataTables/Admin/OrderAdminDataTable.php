ea6afa1c320509cf68f88695752569e046e32edf
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2025-04-07 00:55:04 +0800

    update the name uniq

--- app/DataTables/Admin/OrderAdminDataTable.php
+++ app/DataTables/Admin/OrderAdminDataTable.php
@@ -30,8 +30,8 @@ class OrderAdminDataTable extends DataTable
             ->addIndexColumn()
             ->addColumn('action', function ($item) {
                 return EloquentDataTableBtnElement::button([
-                    'show-btn'   => [ $this->permission . 'read', true, $item->name, route($this->route . 'show', $item->id) ],
-                    'edit-btn'   => [ $this->permission . 'update', true, $item->name, route($this->route . 'edit', $item->id) ],
+                    'show-btn'   => [ $this->permission . 'read', true, $item->uniq, route($this->route . 'show', $item->id) ],
+                    'edit-btn'   => [ $this->permission . 'update', true, $item->uniq, route($this->route . 'edit', $item->id) ],
                     'delete-btn' => [ $this->permission . 'delete', true, route($this->route . 'destroy', $item->id) ]
                 ]);
             })
