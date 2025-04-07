55dad03b9d6d8608abef63d198df24e49b5661d1
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2025-04-07 12:41:37 +0800

    Update

--- resources/views/apps/merchant/orders/show.blade.php
+++ resources/views/apps/merchant/orders/show.blade.php
@@ -4,6 +4,15 @@
 
     <div class="dashboard-table">
 
+        <div class="row">
+            <div class="col-md-7">
+                <h2>Order ID : TXIND953621</h2>
+            </div>
+            <div class="col-md-5">
+                
+            </div>
+        </div>
+
         {!! $order !!}
 
     </div>
