7e51fc4cfde8c884511101c7336895cd1c6ff6e0
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2025-04-07 12:44:35 +0800

    Update the data

--- resources/views/apps/merchant/orders/show.blade.php
+++ resources/views/apps/merchant/orders/show.blade.php
@@ -3,18 +3,37 @@
 @section('user-apps-content')
 
     <div class="dashboard-table">
+        <div class="p-2">
+            <div class="row">
+                <div class="col-md-7">
+                    <h2>Order ID : TXIND953621</h2>
+                </div>
+                <div class="col-md-5">
 
-        <div class="row">
-            <div class="col-md-7">
-                <h2>Order ID : TXIND953621</h2>
+                </div>
             </div>
-            <div class="col-md-5">
-                
+
+            <div class="border border-solid p-2">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h3>Shipping Address</h3>
+                        <p>Double crrl al</p>
+                        <p>123 market street</p>
+                        <p>PA 15632,|</p>
+                        <p>USA</p>
+                    </div>
+                    <div class="col-md-6">
+                        <h3>Shipping Address</h3>
+                        <p>Double crrl al</p>
+                        <p>123 market street</p>
+                        <p>PA 15632,|</p>
+                        <p>USA</p>
+                    </div>
+                </div>
             </div>
+
+            {!! $order !!}
         </div>
-
-        {!! $order !!}
-
     </div>
 
 @endsection
