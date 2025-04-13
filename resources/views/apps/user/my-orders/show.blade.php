89ef2df27e314f25adf85cec318359a10978f767
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2025-04-13 23:05:20 +0800

    Update

--- resources/views/apps/user/my-orders/show.blade.php
+++ resources/views/apps/user/my-orders/show.blade.php
@@ -182,73 +182,64 @@
                         </div>
                     </div>
                 </div>
+
+                <div class="border border-solid p-4">
+                    <div class="row">
+                        <div class="col-md-6 order-detail">
+                            @isset($order->billingAddress)
+                                @php($b = $order->billingAddress)
+                                <h3 class="fw-bold">{!! __('Billing Address') !!}</h3>
+                                <p class="fw-semibold">{!! $b->recipient_name !!}</p>
+                                <p>{!! $b->street_address !!}, {!! $b->address !!}</p>
+                                <p>{!! $b->postcode !!}, {!! $b->city !!}</p>
+                                <p>{!! $b->state !!}, {!! $b->country !!}</p>
+                                <p>{!! $b->phone !!}</p>
+                            @endisset
+                        </div>
+                        <div class="col-md-6 order-detail">
+                            @isset($order->shippingAddress)
+                                @php($s = $order->shippingAddress)
+                                <h3 class="fw-bold">{!! __('Shipping Address') !!}</h3>
+                                <p class="fw-semibold">{!! $s->recipient_name !!}</p>
+                                <p>{!! $s->street_address !!}, {!! $s->address !!}</p>
+                                <p>{!! $s->postcode !!}, {!! $s->city !!}</p>
+                                <p>{!! $s->state !!}, {!! $s->country !!}</p>
+                                <p>{!! $s->phone !!}</p>
+                            @endisset
+                        </div>
+                    </div>
+
+                    <hr class="py-0">
+
+                    <div class="order-detail-item p-3 mb-3">
+                        @forelse($subOrder->items as $j => $item)
+                            <div class="order-itemize d-flex align-items-stretch w-100">
+                                <div class="order-item-img flex-shrink-0 me-3">
+                                    <img src="https://dummyimage.com/600x400/000/fff" alt="Jacket" class="img-fluid">
+                                </div>
+
+                                <div class="order-item-description flex-grow-1">
+                                    <p class="mb-1 text-muted">Jacket</p>
+                                    <h3 class="h5 fw-medium mb-1">{!! $item->product_name !!}</h3>
+                                    <p class="mb-0 text-secondary">Color: Black&nbsp;|&nbsp;Size: XL</p>
+                                </div>
+
+                                <div class="order-item-price flex-shrink-0 text-end ms-auto">
+                                    <p class="mb-0">{!! 'RM' . number_format($item->price, 2) !!} &nbsp;<span class="text-muted"> × {!! $item->quantity !!}</span></p>
+                                </div>
+                            </div>
+                        @empty
+
+                        @endforelse
+                    </div>
+
+                    <hr class="py-0 mt-0">
+                </div>
             @empty
 
             @endforelse
 
             <div class="border border-solid p-4">
-                <div class="row">
-                    <div class="col-md-6 order-detail">
-                        @isset($order->billingAddress)
-                            @php($b = $order->billingAddress)
-                            <h3 class="fw-bold">{!! __('Billing Address') !!}</h3>
-                            <p class="fw-semibold">{!! $b->recipient_name !!}</p>
-                            <p>{!! $b->street_address !!}, {!! $b->address !!}</p>
-                            <p>{!! $b->postcode !!}, {!! $b->city !!}</p>
-                            <p>{!! $b->state !!}, {!! $b->country !!}</p>
-                            <p>{!! $b->phone !!}</p>
-                        @endisset
-                    </div>
-                    <div class="col-md-6 order-detail">
-                        @isset($order->shippingAddress)
-                            @php($s = $order->shippingAddress)
-                            <h3 class="fw-bold">{!! __('Shipping Address') !!}</h3>
-                            <p class="fw-semibold">{!! $s->recipient_name !!}</p>
-                            <p>{!! $s->street_address !!}, {!! $s->address !!}</p>
-                            <p>{!! $s->postcode !!}, {!! $s->city !!}</p>
-                            <p>{!! $s->state !!}, {!! $s->country !!}</p>
-                            <p>{!! $s->phone !!}</p>
-                        @endisset
-                    </div>
-                </div>
-
-                <hr class="py-0">
-
-                <div class="order-detail-item p-3 mb-3">
-                    @forelse($order->subOrders as $i => $subOrder)
-                        @forelse($subOrder->items as $j => $item)
-                        <div class="order-itemize d-flex align-items-stretch w-100">
-                            <div class="order-item-img flex-shrink-0 me-3">
-                                <img src="https://dummyimage.com/600x400/000/fff" alt="Jacket" class="img-fluid">
-                            </div>
-
-                            <div class="order-item-description flex-grow-1">
-                                <p class="mb-1 text-muted">Jacket</p>
-                                <h3 class="h5 fw-medium mb-1">{!! $item->product_name !!}</h3>
-                                <p class="mb-0 text-secondary">Color: Black&nbsp;|&nbsp;Size: XL</p>
-                            </div>
-
-                            <div class="order-item-price flex-shrink-0 text-end ms-auto">
-                                <p class="mb-0">{!! 'RM' . number_format($item->price, 2) !!} &nbsp;<span class="text-muted"> × {!! $item->quantity !!}</span></p>
-                            </div>
-                        </div>
-                        @empty
-
-                        @endforelse
-                    @empty
-                        <div class="order-itemize d-flex align-items-stretch w-100">
-                            <div class="flex-grow-1 text-center py-5">
-                                <h3 class="m-0 text-center">
-                                    No valid items found for this order — they may have been removed or never existed.
-                                </h3>
-                            </div>
-                        </div>
-                    @endforelse
-                </div>
-
-
-
-                <hr class="py-0 mt-0">
 
                 <h4 class="mb-4">Order Summary</h4>
                 <div class="single-cart-item d-flex align-items-stretch">
