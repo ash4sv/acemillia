54394f28e03d1a6d847d85e5c6388d6cb587caba
MUHAMAD ASHRAF <muhamadashraf01@gmail.com>
2025-04-13 14:52:34 +0800

    Update

--- resources/views/apps/user/my-orders/show.blade.php
+++ resources/views/apps/user/my-orders/show.blade.php
@@ -3,12 +3,14 @@
 @section('user-apps-content')
 
     <div class="dashboard-table">
-        <div class="row">
-            <div class="col-md-7">
-                <h3>Order ID : TXIND953621</h3>
-            </div>
-            <div class="col-md-5">
+        <div class="border border-solid p-sm-5 p-2">
+            <div class="row">
+                <div class="col-md-7">
+                    <h3>Order ID : TXIND953621</h3>
+                </div>
+                <div class="col-md-5">
 
+                </div>
             </div>
         </div>
     </div>
