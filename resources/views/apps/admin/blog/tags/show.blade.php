3fe4b1a5492a5d5e99bd23e3ff074d079136c9ad
ash4sv <muhamadashraf01@gmail.com>
2025-08-12 06:13:39 +0800

    Add admin blog tag show view

---
+++ resources/views/apps/admin/blog/tags/show.blade.php
@@ -0,0 +1,67 @@
+<div class="card shadow-none border-1 border-solid">
+    <div class="card-header d-flex justify-content-between align-items-center">
+        <h5 class="mb-0">Tag Details</h5>
+        <div class="btn-group">
+            <a href="{{ route('admin.blog.tags.edit', $postTag->id) }}" class="btn btn-sm btn-primary">Edit</a>
+            <a href="{{ route('admin.blog.tags.index') }}" class="btn btn-sm btn-label-secondary">Back</a>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="row g-4">
+            <div class="col-md-6 text-center">
+                @isset($postTag->image)
+                    <img src="{{ asset($postTag->image) }}" alt="{{ $postTag->name }}" class="img-fluid rounded border" />
+                @endisset
+            </div>
+            <div class="col-md-6">
+                <dl class="row mb-0">
+                    @isset($postTag->id)
+                        <dt class="col-sm-4">ID</dt>
+                        <dd class="col-sm-8">{{ $postTag->id }}</dd>
+                    @endisset
+                    @isset($postTag->name)
+                        <dt class="col-sm-4">Name</dt>
+                        <dd class="col-sm-8">{{ $postTag->name }}</dd>
+                    @endisset
+                    @isset($postTag->slug)
+                        <dt class="col-sm-4">Slug</dt>
+                        <dd class="col-sm-8">{{ $postTag->slug }}</dd>
+                    @endisset
+                    @isset($postTag->description)
+                        <dt class="col-sm-4">Description</dt>
+                        <dd class="col-sm-8">{{ $postTag->description }}</dd>
+                    @endisset
+                    @isset($postTag->status)
+                        <dt class="col-sm-4">Status</dt>
+                        <dd class="col-sm-8">
+                            <span class="badge {{ strtolower($postTag->status) === 'active' ? 'bg-success' : 'bg-label-secondary' }}">
+                                {{ ucfirst($postTag->status) }}
+                            </span>
+                        </dd>
+                    @endisset
+                    @isset($postTag->created_at)
+                        <dt class="col-sm-4">Created At</dt>
+                        <dd class="col-sm-8">{{ $postTag->created_at }}</dd>
+                    @endisset
+                    @isset($postTag->updated_at)
+                        <dt class="col-sm-4">Updated At</dt>
+                        <dd class="col-sm-8">{{ $postTag->updated_at }}</dd>
+                    @endisset
+                </dl>
+            </div>
+        </div>
+        @if($postTag->posts && $postTag->posts->count())
+            <div class="mt-4">
+                <h6>Posts</h6>
+                <ul class="list-unstyled mb-0">
+                    @foreach($postTag->posts as $post)
+                        <li>
+                            <a href="{{ route('admin.blog.posts.show', $post->id) }}">{{ $post->title }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    </div>
+</div>
+
