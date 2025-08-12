a8cb92921a9f3c16d71907971dd1a2248bb5eadd
ash4sv <muhamadashraf01@gmail.com>
2025-08-12 08:01:50 +0800

    refactor: redesign customer detail view

--- resources/views/apps/admin/registered-user/customers/show.blade.php
+++ resources/views/apps/admin/registered-user/customers/show.blade.php
@@ -5,117 +5,129 @@
         :indexRoute="route('admin.registered-user.users.index')"
     />
     <div class="card-body">
-        <div class="mb-3">
-            <label for="name" class="form-label">{!! __('Full Name') !!}</label>
-            <input type="text" name="name" class="form-control" disabled value="{!! old('', $user->name ?? '') !!}">
-        </div>
+        <div class="row g-4">
+            <div class="col-md-6">
+                <dl class="row mb-0">
+                    @isset($user->id)
+                        <dt class="col-sm-4">ID</dt>
+                        <dd class="col-sm-8">{{ $user->id }}</dd>
+                    @endisset
 
-        <div class="row">
-            <div class="col-6">
-                <div class="mb-3">
-                    <label for="email" class="form-label">{!! __('Email') !!}</label>
-                    <input type="text" name="email" class="form-control" disabled value="{!! old('', $user->email ?? '') !!}">
-                </div>
-            </div>
-            <div class="col-6">
-                <div class="mb-3">
-                    <label for="phone" class="form-label">{!! __('Phone') !!}</label>
-                    <input type="text" name="phone" class="form-control" disabled value="{!! old('', $user->phone ?? '') !!}">
-                </div>
-            </div>
-        </div>
+                    @isset($user->name)
+                        <dt class="col-sm-4">Full Name</dt>
+                        <dd class="col-sm-8">{{ $user->name }}</dd>
+                    @endisset
 
-        <div class="row">
-            <div class="col-6">
-                <div class="mb-3">
-                    <label for="date_of_birth" class="form-label">{!! __('Date of Birth') !!}</label>
-                    <input type="text" name="date_of_birth" class="form-control" disabled value="{!! old('', $user->date_of_birth ?? '') !!}">
-                </div>
-            </div>
-            <div class="col-6">
-                <div class="mb-3">
-                    <label for="gender" class="form-label">{!! __('Gender') !!}</label>
-                    <input type="text" name="gender" class="form-control" disabled value="{!! old('', ucfirst($user->gender) ?? '') !!}">
-                </div>
-            </div>
-        </div>
+                    @isset($user->email)
+                        <dt class="col-sm-4">Email</dt>
+                        <dd class="col-sm-8">
+                            <div class="d-flex align-items-center">
+                                {{ $user->email }}
+                                <button type="button" class="btn btn-sm btn-icon btn-outline-secondary ms-2 copy-text" data-copy="{{ $user->email }}" title="Copy Email">
+                                    <i class="ti ti-copy"></i>
+                                </button>
+                            </div>
+                        </dd>
+                    @endisset
 
-        <div class="mb-3">
-            <label for="nationality" class="form-label">{!! __('Nationality') !!}</label>
-            <input type="text" name="nationality" class="form-control" disabled value="{!! old('', $user->nationality ?? '') !!}" data-nationality="{!! $user->nationality ?? '' !!}">
-        </div>
-        <div class="mb-3">
-            <label for="identification_number" class="form-label">{!! __('Identification Number') !!}</label>
-            <input type="text" name="identification_number" class="form-control" disabled value="{!! old('', $user->identification_number ?? '') !!}">
-        </div>
-        <div class="mb-3">
-            <label for="upload_documents" class="form-label">{!! __('Documents') !!}</label>
-            <div class="input-group">
-                <input type="text" name="upload_documents" class="form-control" disabled value="{!! old('', $user->upload_documents ?? '') !!}">
-                <a href="{!! asset($user->upload_documents) !!}" class="btn btn-outline-secondary" target="_blank">View Document</a>
-            </div>
-        </div>
+                    @isset($user->phone)
+                        <dt class="col-sm-4">Phone</dt>
+                        <dd class="col-sm-8">
+                            <div class="d-flex align-items-center">
+                                {{ $user->phone }}
+                                <button type="button" class="btn btn-sm btn-icon btn-outline-secondary ms-2 copy-text" data-copy="{{ $user->phone }}" title="Copy Phone">
+                                    <i class="ti ti-copy"></i>
+                                </button>
+                            </div>
+                        </dd>
+                    @endisset
 
-        @if($user->addressBooks && $user->addressBooks->isNotEmpty())
-            @foreach($user->addressBooks as $address)
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="mb-3">
-                            <label class="form-label">{!! __('Address') !!}</label>
-                            <input type="text" class="form-control" disabled value="{!! $address->address !!}">
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <label class="form-label">{!! __('State') !!}</label>
-                            <input type="text" class="form-control" disabled value="{!! $address->state !!}">
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <label class="form-label">{!! __('City') !!}</label>
-                            <input type="text" class="form-control" disabled value="{!! $address->city !!}">
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <label class="form-label">{!! __('Street') !!}</label>
-                            <input type="text" class="form-control" disabled value="{!! $address->street_address !!}">
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <label class="form-label">{!! __('Postcode') !!}</label>
-                            <input type="text" class="form-control" disabled value="{!! $address->postcode !!}">
-                        </div>
-                    </div>
+                    @isset($user->date_of_birth)
+                        <dt class="col-sm-4">Date of Birth</dt>
+                        <dd class="col-sm-8">{{ $user->date_of_birth }}</dd>
+                    @endisset
+
+                    @isset($user->gender)
+                        <dt class="col-sm-4">Gender</dt>
+                        <dd class="col-sm-8">{{ ucfirst($user->gender) }}</dd>
+                    @endisset
+
+                    @isset($user->nationality)
+                        <dt class="col-sm-4">Nationality</dt>
+                        <dd class="col-sm-8 nationality" data-nationality="{{ $user->nationality }}">{{ $user->nationality }}</dd>
+                    @endisset
+
+                    @isset($user->identification_number)
+                        <dt class="col-sm-4">Identification Number</dt>
+                        <dd class="col-sm-8">{{ $user->identification_number }}</dd>
+                    @endisset
+
+                    @isset($user->upload_documents)
+                        <dt class="col-sm-4">Documents</dt>
+                        <dd class="col-sm-8">
+                            <a href="{{ asset($user->upload_documents) }}" target="_blank">View Document</a>
+                        </dd>
+                    @endisset
+
+                    @isset($user->created_at)
+                        <dt class="col-sm-4">Created At</dt>
+                        <dd class="col-sm-8">{{ $user->created_at }}</dd>
+                    @endisset
+
+                    @isset($user->updated_at)
+                        <dt class="col-sm-4">Updated At</dt>
+                        <dd class="col-sm-8">{{ $user->updated_at }}</dd>
+                    @endisset
+                </dl>
+            </div>
+
+            @if($user->addressBooks && $user->addressBooks->isNotEmpty())
+                <div class="col-md-6">
+                    @foreach($user->addressBooks as $address)
+                        <dl class="row mb-4">
+                            <dt class="col-sm-4">Address</dt>
+                            <dd class="col-sm-8">{{ $address->address }}</dd>
+
+                            <dt class="col-sm-4">State</dt>
+                            <dd class="col-sm-8">{{ $address->state }}</dd>
+
+                            <dt class="col-sm-4">City</dt>
+                            <dd class="col-sm-8">{{ $address->city }}</dd>
+
+                            <dt class="col-sm-4">Street</dt>
+                            <dd class="col-sm-8">{{ $address->street_address }}</dd>
+
+                            <dt class="col-sm-4">Postcode</dt>
+                            <dd class="col-sm-8">{{ $address->postcode }}</dd>
+                        </dl>
+                    @endforeach
                 </div>
-            @endforeach
-        @endif
+            @endif
+        </div>
     </div>
 </div>
 
+@push('script')
 <script>
-    $(document).ready(function(){
-        var $nationalityInput = $('input[name="nationality"]');
-        var nationalityCode = $nationalityInput.data('nationality');
-
-        if(nationalityCode) {
-            $.ajax({
-                url: '/user/countries',
-                method: 'GET',
-                success: function(countries) {
-                    var matchedCountry = countries.find(function(country) {
-                        return country.alpha_2_code === nationalityCode;
-                    });
-                    if(matchedCountry) {
-                        $nationalityInput.val(matchedCountry.en_short_name);
-                    }
-                },
-                error: function(err) {
-                    console.log('Error fetching countries:', err);
-                }
-            });
-        }
+    document.querySelectorAll('.copy-text').forEach(button => {
+        button.addEventListener('click', function () {
+            navigator.clipboard.writeText(this.dataset.copy);
+            this.innerHTML = '<i class="bx bx-check"></i>';
+            setTimeout(() => this.innerHTML = '<i class="ti ti-copy"></i>', 2000);
+        });
     });
+
+    const nationalityEl = document.querySelector('.nationality');
+    if (nationalityEl && nationalityEl.dataset.nationality) {
+        fetch('/user/countries')
+            .then(response => response.json())
+            .then(countries => {
+                const match = countries.find(country => country.alpha_2_code === nationalityEl.dataset.nationality);
+                if (match) {
+                    nationalityEl.textContent = match.en_short_name;
+                }
+            })
+            .catch(err => console.error('Error fetching countries:', err));
+    }
 </script>
+@endpush
